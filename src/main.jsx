11acfdfd06e6fd5439489ee2aa9f7a263b1359b2
Nikita <came95@bk.ru>
2024-07-15 17:30:36 +0300

    minor edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./assets/scss/normalize.scss";
-import '/src/assets/scss/index.scss'
+import "./assets/global/normalize.scss";
+import '/src/assets/global/index.scss'
 
 import Banner from "./components/BannerSection/Banner.jsx";
 import ChuteSection from "./components/ChuteSection/Chute.jsx";
